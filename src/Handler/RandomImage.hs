74ccbd8954c09c95b076a063fc6b758a40e18897
Anton Haubner <anton.haubner@outlook.de>
2018-06-17 20:33:48 +0200

    mremoved temporary fixes of the svg output, since I applied them to the PolygonArt main library

--- src/Handler/RandomImage.hs
+++ src/Handler/RandomImage.hs
@@ -17,8 +17,7 @@ getRandomImageR seed = sendResponse (typeSvg, toContent image)
   where
     image :: Text
     image = (
-          (Data.Text.replace "<svg" "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"")
-        . fromString
+          fromString
         . generateImage
       )
         seed
